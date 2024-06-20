6f2c91bbcdb0c3d114112ba0557ecb4595bbfebb
mrsterner <simon@sterner.dev>
2024-06-20 09:55:01 +0200

    fix #118

--- src/main/java/dev/sterner/guardvillagers/common/entity/goal/RangedCrossbowAttackPassiveGoal.java
+++ src/main/java/dev/sterner/guardvillagers/common/entity/goal/RangedCrossbowAttackPassiveGoal.java
@@ -156,7 +156,7 @@ public class RangedCrossbowAttackPassiveGoal<T extends PathAwareEntity & RangedA
                 this.mob.shootAt(livingentity, 1.0F);
                 ItemStack itemstack1 = this.mob.getStackInHand(GuardVillagers.getHandWith(this.mob, item -> item instanceof CrossbowItem));
                 ((LivingEntity)livingentity).setCurrentHand(ProjectileUtil.getHandPossiblyHolding(livingentity, Items.CROSSBOW));
-                ((CrossbowUser)livingentity).setCharging(false);
+                ((CrossbowUser)this.mob).setCharging(false);
                 this.crossbowState = CrossbowState.UNCHARGED;
             }
         }
