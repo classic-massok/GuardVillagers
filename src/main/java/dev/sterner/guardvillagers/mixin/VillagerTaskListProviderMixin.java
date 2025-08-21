7301cc59bb2f1f965f7ce8d2b019a5c9326e5dc9
Marcus Massok <m.c.massok@gmail.com>
2025-08-21 00:38:27 -0400

    reference correct var name

--- src/main/java/dev/sterner/guardvillagers/mixin/VillagerTaskListProviderMixin.java
+++ src/main/java/dev/sterner/guardvillagers/mixin/VillagerTaskListProviderMixin.java
@@ -35,7 +35,7 @@ public class VillagerTaskListProviderMixin {
     @Inject(method = "createIdleTasks", cancellable = true, at = @At("RETURN"))
     private static void guardvillagers$createIdleTasks(RegistryEntry<VillagerProfession> profession, float speed, CallbackInfoReturnable<ImmutableList<Pair<Integer, ? extends Task<? super VillagerEntity>>>> cir) {
         List<Pair<Integer, ? extends Task<? super VillagerEntity>>> villagerList = new ArrayList<>(cir.getReturnValue());
-        villagerList.add(Pair.of(2, new RandomTask<>(ImmutableList.of(Pair.of(FindEntityTask.create(GuardVillagers.GUARD_VILLAGER, 8, MemoryModuleType.INTERACTION_TARGET, pSpeedModifier, 2), 3), Pair.of(FindEntityTask.create(EntityType.VILLAGER, 8, MemoryModuleType.INTERACTION_TARGET, pSpeedModifier, 2), 3), Pair.of(new WaitTask(30, 60), 1)))));
+        villagerList.add(Pair.of(2, new RandomTask<>(ImmutableList.of(Pair.of(FindEntityTask.create(GuardVillagers.GUARD_VILLAGER, 8, MemoryModuleType.INTERACTION_TARGET, speed, 2), 3), Pair.of(FindEntityTask.create(EntityType.VILLAGER, 8, MemoryModuleType.INTERACTION_TARGET, pSpeedModifier, 2), 3), Pair.of(new WaitTask(30, 60), 1)))));
         villagerList.add(Pair.of(2, new CompositeTask<>(ImmutableMap.of(), ImmutableSet.of(MemoryModuleType.INTERACTION_TARGET), CompositeTask.Order.ORDERED, CompositeTask.RunMode.RUN_ONE, ImmutableList.of(Pair.of(new ShareGossipWithGuard(), 1), Pair.of(new GatherItemsVillagerTask(), 1)))));
         cir.setReturnValue(ImmutableList.copyOf(villagerList));
     }
